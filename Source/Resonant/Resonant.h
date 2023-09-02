8d5d09fb1ba6493b88db01ee0bb55f1f8a7e4776
Seth Crowther <sethcrowther9@gmail.com>
2023-09-02 16:22:49 +0100

    Changed Attack objects to structs.

--- Source/Resonant/Resonant.h
+++ Source/Resonant/Resonant.h
@@ -3,4 +3,46 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
+#include "Resonant.generated.h"
 
+UENUM(BlueprintType)
+enum AttackHeight
+{
+	None,
+	Low,
+	High,
+	Aerial
+};
+
+USTRUCT(BlueprintType)
+struct FAttack
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UAnimMontage* animation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FName targetBone;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool immobilizeHorizontal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool immobilizeVertical;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TEnumAsByte<AttackHeight> height;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		UInputAction* inputAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float beatsToStun;
+};
